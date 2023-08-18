f3102a8894441776a4eeb95989c9c60712603ed9
Daniel Weiner <danielbweiner@gmail.com>
2023-08-18 17:33:45 -0500

    More logging, fix initial event

--- lambda-layer/self-invoke-on-shutdown/extension.py
+++ lambda-layer/self-invoke-on-shutdown/extension.py
@@ -8,12 +8,12 @@ import sys
 from pathlib import Path
 
 LAMBDA_EXTENSION_NAME = Path(__file__).parent.name
-LAMBDA_FUNCTION_NAME=os.getenv("AWS_LAMBDA_FUNCTION_NAME")
+LAMBDA_FUNCTION_NAME = os.getenv("AWS_LAMBDA_FUNCTION_NAME")
 LAMBDA_FUNCTION_VERSION = os.getenv("AWS_LAMBDA_FUNCTION_VERSION")
-LAMBDA_RUNTIME_API=os.getenv("AWS_LAMBDA_RUNTIME_API")
-BASE_RUNTIME_API_URL=f"http://{LAMBDA_RUNTIME_API}/2020-01-01/extension"
-REGISTER_URL=f"{BASE_RUNTIME_API_URL}/register"
-NEXT_EVENT_URL=f"{BASE_RUNTIME_API_URL}/event/next"
+LAMBDA_RUNTIME_API = os.getenv("AWS_LAMBDA_RUNTIME_API")
+BASE_RUNTIME_API_URL = f"http://{LAMBDA_RUNTIME_API}/2020-01-01/extension"
+REGISTER_URL = f"{BASE_RUNTIME_API_URL}/register"
+NEXT_EVENT_URL = f"{BASE_RUNTIME_API_URL}/event/next"
 
 def log(output: str):
     print(f"[{LAMBDA_EXTENSION_NAME}] {output}", flush=True)
@@ -40,18 +40,18 @@ def register_extension():
 
 def self_invoke():
     if os.getenv("SPACY_SERVER_ENV", "dev") == "prod":
-        
         lambda_client = boto3.client("lambda")
+        log("Getting latest function definition.")
         latest_function = lambda_client.get_function(
-            FunctionName=f"{LAMBDA_FUNCTION_NAME}",
+            FunctionName=LAMBDA_FUNCTION_NAME,
             Qualifier='latest'
         )
         version = latest_function['Configuration']['Version']
-        log(f"Latest function version: {version}. Current function version: {LAMBDA_FUNCTION_VERSION}")
+        log(f"Latest function version: {version}. Current function version: {LAMBDA_FUNCTION_VERSION}.")
         if version == LAMBDA_FUNCTION_VERSION:
             log(f"Invoking lambda function version {version}.")
             lambda_client.invoke(
-                FunctionName=f"{LAMBDA_FUNCTION_NAME}",
+                FunctionName=LAMBDA_FUNCTION_NAME,
                 InvocationType="Event",
                 Payload=json.dumps({ 
                     "warmup": True
